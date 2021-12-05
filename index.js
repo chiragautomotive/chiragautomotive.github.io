e97a85c0a3a93525ec7fa67dac3047ef302fa40b
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2021-12-05 18:50:06 +0530

    Update index.js
    bruh

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function getPhoneNumber(){
             // Selecting the input element and get its value 
-            var inputVal = document.getElementById("phoneNumber").value;
-          
+            var inputVal = 
+            
+            document.getElementById("phoneNumber").value;
+        var walink ="https://wa.me/"+inputVal  
     document.getElementById("link").innerHTML = "https://wa.me/"+inputVal; 
   
-  a.setAttribute('href', "https://wa.me/"+inputVal);
+ document.getElementById("link").setAttribute("href", "https://wa.me/"+inputVal)
 }
