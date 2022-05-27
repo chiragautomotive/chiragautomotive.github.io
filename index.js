7c26474c9d56163980564d161040e0066647113a
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2022-05-27 19:53:42 +0530

    Add files via upload
    update whatsapp

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function getPhoneNumber(){
             // Selecting the input element and get its value 
-            var inputVal = 
-            
-            document.getElementById("phoneNumber").value;
-        var walink ="https://wa.me/"+inputVal  
-    document.getElementById("link").innerHTML = "https://wa.me/"+inputVal; 
+            var inputVal = 91+document.getElementById("phoneNumber").value;
+        var walink ="https://wa.me/"+inputVal
+    document.getElementById("link").innerHTML = "https://wa.me/"+inputVal
+ 
   
- document.getElementById("link").setAttribute("href", "https://wa.me/"+inputVal)
+ document.getElementById("link").setAttribute("href", "https://wa.me/"+inputVal+"?text=Dear%20Sir%2FMam%2C%0AYour%20number%20plate%20is%20ready%20for%20fitting.%20Kindly%20visit%20our%20showroom%20for%20number%20plate%20fitting.%20%0AChirag%20Automotive%20%0AGF%20-%2023%2F24%20Earth%20Eon%20Opp%20sama%20lake%20opp%20urmi%20school%20sama%20savli%20road%20%2C%20Vadodara.%0ATiming%3A%2010%20am%20to%201%20pm%2C%203%20pm%20to%205%20pm%0AKindly%20contact%207041756888%20for%20more%20details")
 }
