0a54ef4bb8490667a3bcf278bbb12780fae5b514
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2022-05-29 10:18:45 +0530

    Add files via upload
    v 1.13 message update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getPhoneNumber(){
             var inputVal = 91+document.getElementById("phoneNumber").value;
         var walink ="https://wa.me/"+inputVal
  
-  window.open("https://wa.me/"+inputVal+"?text=Dear%20Sir%2FMam%2C%0AYour%20vehicle%20number%20plate%20is%20ready%20for%20fitting.%20Kindly%20visit%20our%20showroom%20for%20your%20vehicle%20number%20plate%20fitting.%20%0AChirag%20Automotive%20%0AGF%20-%2023%2F24%20Earth%20Eon%20Opp%20sama%20lake%20opp%20urmi%20school%20sama%20savli%20road%20%2C%20Vadodara.%0ATiming%3A%2010%20am%20to%201%20pm%2C%203%20pm%20to%205%20pm%0AKindly%20contact%207041756888%20for%20more%20details")
+  window.open("https://wa.me/"+inputVal+"?text=Dear%20Sir%2FMam%2C%0AYour%20vehicle%20number%20plate%20is%20ready%20for%20fitting.%20Kindly%20visit%20our%20showroom%20for%20your%20vehicle%20number%20plate%20fitting.%20%0AChirag%20Automotive%20%0AGF%20-%2023%2F24%20Earth%20Eon%20Opp%20sama%20lake%20opp%20urmi%20school%20sama%20savli%20road%20%2C%20Vadodara.%0ATiming%3A%20MONDAY%20-%20SATURDAY%0A10%20am%20to%201%20pm%2C%203%20pm%20to%205%20pm%0AKindly%20contact%207041756888%20for%20more%20details")
 }
